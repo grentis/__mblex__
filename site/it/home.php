3b21bb3344d60cb6a5cb9a59fbdf6e976d66c7b1
grentis <grentis@gmail.com>
2011-01-25 17:53:18 +0100

    ciao

---
+++ site/it/home.php
@@ -0,0 +1,105 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Home | Studio Legale Bicecci & Partners</title>
+		<meta name="description" content="Lo Studio Legale Bicecci & Partners fonde la ventennale esperienza professionale e le specifiche competenze di ciascun partner in diritto bancario, finanziario e societario, in ambito giudiziale e stragiudiziale" />
+		<meta name="keywords" content="studio legale, bicecci, blex, diritto bancario, diritto finanziario, diritto societario" />
+		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+		<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.3.0/build/cssreset/reset-min.css">
+		<link rel="stylesheet" href="../resources/style.css" type="text/css">
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+	</head>
+	<!--[if IE]>
+		<body class="it ie">
+	<![endif]-->
+	<!--[if !IE]>
+		<body class="it">
+	<![endif]-->
+			<div class="container">
+				<div class="main">
+					<header>
+						<div class="header">
+							<nav>
+								<ul class="nav">
+									<li class="studio first"><a href="studio.php"><span>Studio</span></a></li>
+									<li class="expertise"><a href="expertise.php"><span>Expertise</span></a></li>
+									<li class="network"><a href="network.php"><span>Network</span></a></li>
+									<li class="info"><a href="info.php"><span>Info</span></a></li>
+									<li class="workwithus"><a href="workwithus.php"><span>Lavora con noi</span></a></li>
+									<li class="contacts last"><a href="contacts.php"><span>Contatti</span></a></li>
+								</ul>
+							</nav>
+							<a class="logo" href="home.php">Logo</a>
+							<img class="main-banner" src="../resources/images/banner.jpg" />
+						</div>
+					</header>
+					<section>
+						<div class="section">
+							<aside>
+								<div class="aside">
+									<div class="news">
+										<h3>Alert informativi<a href="info.php"></a></h3>
+										<article>
+											<div class="article first">
+												<h4><a href="info.php">Primo informativo</a></h4>
+												<p>Lo studio nasce fondendo la ventennale esperienza professionale e le specifiche...</p>
+											</div>
+										</article>
+										<article>
+											<div class="article last">
+												<h4><a href="info.php">Secondo informativo</a></h4>
+												<p>Lo studio nasce fondendo la ventennale esperienza professionale e le specifiche...</p>
+											</div>
+										</article>
+									</div>
+								</div>
+							</aside>
+							<div class="content">
+								<h2 class="first">Bicecci & Partners</h2>
+								<p>
+									Il nostro Studio è ispirato ai più profondi valori professionali e personali: indipendenza, orientamento ai risultati, dinamicità, flessibilità.
+								</p>
+								<p>
+									La nostra capacità di cogliere ogni più piccolo dettaglio senza mai perdere la visione d’insieme, la nostra attitudine a saperci adattare alle esigenze del cliente e, non ultima, la nostra propensione ad instaurare rapporti fiduciari durevoli nel tempo, fanno del nostro Studio una vera e propria boutique dove la clientela trova la risposta giusta e calibrata alle proprie esigenze.
+								</p>
+								<h2>Visione e missione</h2>
+								<p>
+									Un amico serio e riservato al fianco del cliente. Lo studio legale Bicecci&Partners si propone come una realtà di riconosciuta esperienza, rinomata per competenza, serietà e disponibilità dei professionisti.
+								</p>
+								<p>
+									Grazie alla struttura moderna, dinamica e veloce ed alle sinergie professionali e personali, i professionisti dello Studio Legale Bicecci&Partners sono in grado di accogliere e curare le molteplici richieste di banche, società finanziarie, SIM, SGR, società quotate e non e piccole e medie imprese
+								</p>
+							</div>
+							<div class="clear"></div>
+						</div>
+					</section>
+				</div>
+				<footer>
+					<div class="footer">
+						<nav>
+							<ul class="nav">
+								<li class="home first current"><a href="home.php"><span>HOME</span></a></li>
+								<li class="studio"><a href="studio.php"><span>STUDIO</span></a></li>
+								<li class="expertise"><a href="expertise.php"><span>EXPERTISE</span></a></li>
+								<li class="network"><a href="network.php"><span>NETWORK</span></a></li>
+								<li class="info"><a href="info.php"><span>INFO</span></a></li>
+								<li class="workwithus"><a href="workwithus.php"><span>LAVORA CON NOI</span></a></li>
+								<li class="contacts"><a href="contacts.php"><span>CONTATTI</span></a></li>
+								<li class="privacy"><a href="privacy.php"><span>PRIVACY</span></a></li>
+								<li class="disclaimer last"><a href="disclaimer.php"><span>DISCLAIMER</span></a></li>
+							</ul>
+						</nav>
+						<div class="company-details">
+							Studio Legale Bicecci &amp; Partners - Via Baretti, 1 (ang. P.za Duse) 20122 Milano - Tel. 02 89 658 658 - Fax 02 45 495 819<br/>
+							Viale Bruno Buozzi, 53 - 00197 Roma -  info@mblex.it - P.IVA 12519000157
+						</div>
+					</div>
+				</footer>
+			</div>
+		</body>
+</html>
+
+
+<!--
+usare <article> <time> per le news
+-->
